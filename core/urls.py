3fe7d0c8ffc8a174dd76e5c82f49ee119bb2b864
yanswo <yanlucas2202@gmail.com>
2025-09-21 14:37:20 -0300

    initial setup

--- core/urls.py
+++
@@ -1,7 +0,0 @@
-# core/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.listar_pessoas, name='listar_pessoas'),
-]
--- core/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Pessoa
-
-def listar_pessoas(request):
-    pessoas = Pessoa.objects.select_related('usuario').prefetch_related('endereco_set')
-    contexto = {
-        'pessoas': pessoas
-    }
-    return render(request, 'index.html', contexto)
